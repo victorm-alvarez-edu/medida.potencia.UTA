1607c70069901033330c927578a3c6f0c20f6434
victorm-alvarez-edu <victorm.alvarez.edu@gmail.com>
2021-06-20 17:18:10 +0200

    Versión definitiva, incuído mensaxe de erro en LCD.

--- medir.potencia.termica.lcd.ino
+++ medir.potencia.termica.lcd.ino
@@ -9,15 +9,12 @@
 
 #define PIN_DHT1 A0
 #define PIN_DHT2 A1
-//#define PIN_DHT3 A2
 
 int tempo = 7000;
   DHT sensor1(PIN_DHT1, DHT11);  //Declaramos obxecto dht
   DHT sensor2(PIN_DHT2, DHT11);  //Declaramos obxecto dht
-//  DHT sensor3(PIN_DHT3, DHT11);  //Declaramos obxecto dht
 float humidade1, temperatura1;
 float humidade2, temperatura2;
-//float humidade3, temperatura3;
 String mensaxe = "";
   LiquidCrystal_I2C lcd(0x27, 16, 2); //Se o LCD non mostra datos, probar enderezo: 0x3F
 
@@ -25,39 +22,37 @@ void setup() {
   Serial.begin(9600);
   sensor1.begin();
   sensor2.begin();
-//  sensor3.begin();
   lcd.begin();
   lcd.backlight();
   lcd.setCursor(0, 0);
 }
 
 void loop() {
+  delay(tempo/4); //Espera aínda que se produza un erro de lectura
+  
   humidade1 = sensor1.readHumidity();
   temperatura1 = sensor1.readTemperature();
   humidade2 = sensor2.readHumidity();
   temperatura2 = sensor2.readTemperature();
-//  humidade3 = sensor3.readHumidity();
-//  temperatura3 = sensor3.readTemperature();
 
   //Detección de erros
+  lcd.clear();
   if(isnan(humidade1) || isnan(temperatura1)) {
     Serial.println("Fallo na lectura do DHT11 #01");
+    lcd.setCursor(0, 0);
+    lcd.print("#1: Fallo sensor");
     return;
   }
   if(isnan(humidade2) || isnan(temperatura2)) {
     Serial.println("Fallo na lectura do DHT11 #02");
+    lcd.setCursor(0, 1);
+    lcd.print("#2: Fallo sensor");
     return;
   }
-//    if(isnan(humidade3) || isnan(temperatura3)) {
-//    Serial.println("Fallo na lectura do DHT11 #03");
-//    return;
-//  }
   mensaxe = "Sensor #01: Humidade:\t" + String(humidade1) + " %\t\tTemperatura:\t" + String(temperatura1) + " ºC\n";
   Serial.print(mensaxe);
   mensaxe = "Sensor #02: Humidade:\t" + String(humidade2) + " %\t\tTemperatura:\t" + String(temperatura2) + " ºC\n";
   Serial.print(mensaxe);
-//  mensaxe = "Sensor #03: Humidade:\t" + String(humidade3) + " %\t\tTemperatura:\t" + String(temperatura3) + " ºC\n";
-//  Serial.print(mensaxe);
   Serial.println("");
   
   // Mostrar mensaxe no LCD
@@ -69,12 +64,4 @@ void loop() {
   lcd.print(mensaxe); // Linha #02
   delay(tempo);
   lcd.clear();
-//  lcd.setCursor(0, 0);
-//  lcd.print(mensaxe); // Linha #01
-//  mensaxe = "#3 " + String(humidade3, 1) + " % " + String(temperatura3, 1) + " C"; 
-//  lcd.setCursor(0, 1);
-//  lcd.print(mensaxe); // Linha #02
-//  delay(tempo);
-//  lcd.clear();
-  delay(tempo/4);
 }
